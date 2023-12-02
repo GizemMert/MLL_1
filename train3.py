6ed68d88f101a01b27aeac4dcd5113a02892a6de
GizemMert <gizem.mert12@gmail.com>
2023-12-02 08:53:36 +0100

    updated

--- train3.py
+++ train3.py
@@ -40,24 +40,24 @@ cff_feat_rec = 0.30
 cff_im_rec = 0.60
 cff_class = 0.10
 
-beta = 200
+beta = 4
 
-umap_dir = 'umap_figures'
+umap_dir = 'umap_figures3'
 if not os.path.exists(umap_dir):
     os.makedirs(umap_dir)
 
-latent_dir = 'latent_data'
+latent_dir = 'latent_data3'
 if not os.path.exists(latent_dir):
     os.makedirs(latent_dir)
 
-result_dir = "training_results"
+result_dir = "training_results3"
 os.makedirs(result_dir, exist_ok=True)
-result_file = os.path.join(result_dir, "training_results.txt")
+result_file = os.path.join(result_dir, "training_results3.txt")
 
 for epoch in range(epochs):
     loss = 0
     acc_imrec_loss = 0
-    acc_featrec_loss = 0
+    # acc_featrec_loss = 0
     kl_div_loss = 0
     y_true = []
     y_pred = []
@@ -79,16 +79,16 @@ for epoch in range(epochs):
 
         z_dist, output, im_out, mu, log_var = model(feat)
 
-        feat_rec_loss = criterion(output, feat)
-        imrec_loss = 1 - criterion_1(im_out, scimg)
+        # feat_rec_loss = criterion(output, feat)
+        imrec_loss = criterion(im_out, scimg)
         kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0)
-        train_loss = feat_rec_loss + imrec_loss + (beta * kld_loss)
+        train_loss = imrec_loss + (beta * kld_loss)
 
         train_loss.backward()
         optimizer.step()
 
         loss += train_loss.data.cpu()
-        acc_featrec_loss += feat_rec_loss.data.cpu()
+        # acc_featrec_loss += feat_rec_loss.data.cpu()
         acc_imrec_loss += imrec_loss.data.cpu()
         kl_div_loss += kld_loss.data.cpu()
 
@@ -101,16 +101,16 @@ for epoch in range(epochs):
         # y_pred.extend(predicted.cpu().numpy())
 
     loss = loss / len(train_dataloader)
-    acc_featrec_loss = acc_featrec_loss / len(train_dataloader)
+    # acc_featrec_loss = acc_featrec_loss / len(train_dataloader)
     acc_imrec_loss = acc_imrec_loss / len(train_dataloader)
     kl_div_loss = kl_div_loss / len(train_dataloader)
     # f1 = f1_score(y_true, y_pred, average='weighted')
 
-    print("epoch : {}/{}, loss = {:.6f}, feat_loss = {:.6f}, imrec_loss = {:.6f}, kl_div = {:.6f}".format
-          (epoch + 1, epochs, loss, acc_featrec_loss, acc_imrec_loss, kl_div_loss))
+    print("epoch : {}/{}, loss = {:.6f}, imrec_loss = {:.6f}, kl_div = {:.6f}".format
+          (epoch + 1, epochs, loss, acc_imrec_loss, kl_div_loss))
 
     with open(result_file, "a") as f:
-        f.write(f"Epoch {epoch + 1}: Loss = {loss:.6f}, Feat_Loss = {acc_featrec_loss:.6f}, "
+        f.write(f"Epoch {epoch + 1}: Loss = {loss:.6f}, "
                 f"Img_Rec_Loss = {acc_imrec_loss:.6f}, KL_DIV = {kl_div_loss:.6f} \n")
 
     if epoch % 10 == 0:
@@ -118,7 +118,7 @@ for epoch in range(epochs):
         np.save(latent_filename, np.concatenate(all_means, axis=0))
 
     model.eval()
-
+    """
     if epoch % 10 == 0:
         # Load all latent representations
         latent_data = np.load(latent_filename)
@@ -152,6 +152,7 @@ for epoch in range(epochs):
         # Save the UMAP figure
         plt.savefig(umap_figure_filename, dpi=300)
         plt.close()
+    """
 
     for i in range(30):
         ft, img, lbl, _ = train_dataset[i]
@@ -167,14 +168,14 @@ for epoch in range(epochs):
         im = np.concatenate([img, im_out], axis=1)
 
         if epoch % 10 == 0:
-            file_name = "reconsructed-images/"
+            file_name = "reconsructed-images3/"
             if os.path.exists(os.path.join(file_name)) is False:
                 os.makedirs(os.path.join(file_name))
             cv2.imwrite(os.path.join(file_name, str(i) + "-" + str(epoch) + ".jpg"), im * 255)
 
 script_dir = os.path.dirname(__file__)
 
-model_save_path = os.path.join(script_dir, 'trained_model.pth')
+model_save_path = os.path.join(script_dir, 'trained_model3.pth')
 torch.save(model.state_dict(), model_save_path)
 print(f"Trained model saved to {model_save_path}")
 
