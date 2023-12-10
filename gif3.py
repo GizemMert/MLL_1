e4611fdd7d5434fc97abc7110b3ed35b502533f4
GizemMert <gizem.mert12@gmail.com>
2023-12-10 18:29:41 +0100

    updated

--- gif3.py
+++ gif3.py
@@ -1,15 +1,15 @@
 from PIL import Image
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
-import os
 from model4 import VariationalAutoencodermodel4, reparametrize
-from Dataloader import Dataloader, label_map
+from Dataloader import Dataloader
 from torch.utils.data import DataLoader
-
 
 def interpolate_gif(model, filename, features, n=100, latent_dim=30):
     model.eval()
+
+    # Define the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Function to extract the latent vector from the model output
     def get_latent_vector(x):
@@ -19,40 +19,51 @@ def interpolate_gif(model, filename, features, n=100, latent_dim=30):
         z = reparametrize(mu, logvar)
         return z
 
+    # Generate the latent representations
     latents = [get_latent_vector(feature.to(device)) for feature in features]
 
+    # Interpolate between the latent vectors
     all_interpolations = []
     for i in range(len(latents) - 1):
-        z1, z2 = latents[i], latents[i + 1]
-        # Generate interpolated latent vectors
         for t in np.linspace(0, 1, n):
-            z_interp = z1 * (1 - t) + z2 * t
+            z_interp = latents[i] * (1 - t) + latents[i + 1] * t
             all_interpolations.append(z_interp)
 
-    # Decode the interpolated latent vectors
-    interpolate_list = [model.decoder(z).squeeze(0) for z in all_interpolations]
-    interpolate_list = [model.img_decoder(z).squeeze(0) for z in interpolate_list]
-    interpolate_list = [z.permute(1, 2, 0).to('cpu').detach().numpy() * 255 for z in interpolate_list]
+    # Decode the latent vectors and generate images
+    interpolate_list = []
+    for z in all_interpolations:
+        y = model.decoder(z)
+        img = model.img_decoder(y)
+        img = img.permute(0, 2, 3, 1)  # Assuming the output is [batch_size, channels, height, width]
+        interpolate_list.append(img.squeeze(0).to('cpu').detach().numpy())
+
+    # Normalize the pixel values to be between 0 and 255 for image saving
+    interpolate_list = [np.clip(img * 255, 0, 255).astype(np.uint8) for img in interpolate_list]
 
     # Convert to PIL images and resize
-    images_list = [Image.fromarray(img.astype(np.uint8)).resize((256, 256)) for img in interpolate_list]
+    images_list = [Image.fromarray(img) for img in interpolate_list]
     images_list = images_list + images_list[::-1]  # Loop back to the beginning
 
-    # Save as a GIF
+    # Save as a GIF with a duration between frames of 100ms
     images_list[0].save(
         f'{filename}.gif',
         save_all=True,
         append_images=images_list[1:],
-        loop=1)
+        loop=0,  # Loop forever
+        duration=100  # Duration between frames in milliseconds
+    )
 
-
+# Load the model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# Load your model
 model = VariationalAutoencodermodel4(latent_dim=30)
 model_save_path = 'trained_model4cp2.pth'
-model.load_state_dict(torch.load(model_save_path, map_location='cuda' if torch.cuda.is_available() else 'cpu'))
-model.to('cuda' if torch.cuda.is_available() else 'cpu')
+model.load_state_dict(torch.load(model_save_path, map_location=device))
+model.to(device)
 model.eval()
+
+# Extract features from different classes
+train_dataset = Dataloader(split='train')
+train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=1)
 
 
 def get_images_from_different_classes(dataloader, num_classes=4):
