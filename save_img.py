c8f71e40aa021c5e4867a25c949275a6107421b8
GizemMert <gizem.mert12@gmail.com>
2023-12-13 12:58:52 +0100

    updated

--- save_img.py
+++ save_img.py
@@ -1,24 +1,28 @@
 import cv2
 import os
+import numpy as np
 from Dataloader import Dataloader, label_map
 from torch.utils.data import DataLoader
-
-import numpy as np
 
 
 def save_class_images(dataloader, classes_of_interest, save_directory):
     # Dictionary to track whether an image for each class has been saved
     saved_classes = {class_label: False for class_label in classes_of_interest}
 
-    for _, roi_cropped, label, _ in dataloader:
+    for _, roi_cropped, label_tensor, _ in dataloader:
+        # Convert label tensor to Python integer
+        label = label_tensor.item()  # Assuming label_tensor is a single-element tensor
+
         if label in classes_of_interest and not saved_classes[label]:
+            # Constructing the filename with label information
+            label_name = [name for name, number in label_map.items() if number == label][0]
+            filename = os.path.join(save_directory, f"class_{label_name}_image.png")
+
             # Save the roi_cropped image
-            filename = os.path.join(save_directory, f"{label}.png")
             roi_cropped_image = np.transpose(roi_cropped, (1, 2, 0))  # Convert to HWC format for saving
             cv2.imwrite(filename, roi_cropped_image * 255)  # Rescale back to 0-255 range
             saved_classes[label] = True
 
-            # Check if all classes are saved
             if all(saved_classes.values()):
                 break
 
@@ -32,3 +36,4 @@ classes_of_interest = [label_map['myeloblast'], label_map['basophil'], label_map
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 save_class_images(train_dataloader, classes_of_interest, current_directory)
+
