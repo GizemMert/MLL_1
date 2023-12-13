89a6098b7f936fef8bb607f8133b65650efffde6
GizemMert <gizem.mert12@gmail.com>
2023-12-13 13:11:13 +0100

    updated

--- save_img.py
+++ save_img.py
@@ -10,22 +10,22 @@ def save_class_images(dataloader, classes_of_interest, save_directory):
     saved_classes = {class_label: False for class_label in classes_of_interest}
 
     for _, roi_cropped, label_tensor, _ in dataloader:
-        # Convert label tensor to Python integer
-        label = label_tensor.item()  # Assuming label_tensor is a single-element tensor
-
+        label = label_tensor.item()  # Convert label tensor to Python integer
         if label in classes_of_interest and not saved_classes[label]:
+            # Convert from CHW to HWC format
+            roi_cropped_hwc = np.transpose(roi_cropped, (1, 2, 0))
+
             # Constructing the filename with label information
             label_name = [name for name, number in label_map.items() if number == label][0]
             filename = os.path.join(save_directory, f"class_{label_name}_image.png")
 
             # Save the roi_cropped image
-            roi_cropped_image = np.transpose(roi_cropped, (1, 2, 0))  # Convert to HWC format for saving
-            cv2.imwrite(filename, roi_cropped_image * 255)  # Rescale back to 0-255 range
+            cv2.imwrite(filename, roi_cropped_hwc * 255)  # Rescale back to 0-255 range
             saved_classes[label] = True
 
+            # Check if all classes are saved
             if all(saved_classes.values()):
                 break
-
 
 # Usage
 train_dataset = Dataloader(split='train')
@@ -36,4 +36,5 @@ classes_of_interest = [label_map['myeloblast'], label_map['basophil'], label_map
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 save_class_images(train_dataloader, classes_of_interest, current_directory)
+
 
