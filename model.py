4b8599c03af2278c6d1e12e44391b0308a1ef9dc
GizemMert <gizem.mert12@gmail.com>
2023-12-19 18:39:29 +0100

    updated

--- model.py
+++ model.py
@@ -46,13 +46,13 @@ class VariationalAutoencodermodel(nn.Module):
         )
 
         self.img_decoder = nn.Sequential(
-            nn.ConvTranspose2d(256, 96, kernel_size=5, stride=2),
+            nn.ConvTranspose2d(256, 96, kernel_size=4, stride=3),
             nn.ReLU(),
-            nn.ConvTranspose2d(96, 48, kernel_size=3, stride=2),
+            # nn.ConvTranspose2d(96, 48, kernel_size=3, stride=2),
+            # nn.ReLU(),
+            nn.ConvTranspose2d(96, 24, kernel_size=2, stride=3),
             nn.ReLU(),
-            nn.ConvTranspose2d(48, 24, kernel_size=3, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(24, 3, kernel_size=2),
+            nn.ConvTranspose2d(24, 3, kernel_size=1),
             #nn.ReLU(),
             #nn.ConvTranspose2d(16, 3, kernel_size=1),
             nn.Sigmoid()
