0bd4591fc88127ef5d1ecb64d5da936f098435d3
GizemMert <gizem.mert12@gmail.com>
2024-01-16 18:29:35 +0100

    updated

--- pkl.py
+++ pkl.py
@@ -1,30 +1,18 @@
-import gzip
-import pickle
-import os
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from Dataloader import Dataloader, label_map
 
-# Define the path to your .dat.gz file
-file_path = "/lustre/groups/aih/raheleh.salehi/Master-thesis/Aug_features_datasets/Augmented-MLL-AML_MLLdataset.dat.gz"
 
-# Open the gzip compressed file
-with gzip.open(os.path.join(file_path), "rb") as f:
-    data = pickle.load(f)
 
-# Check the type of the loaded data
-print(type(data))
+epochs = 300
+batch_size = 128
 
-# If it is a dictionary, iterate over its keys
-if isinstance(data, dict):
-    print("Keys in the file:", data.keys())
+# Load the dataset
+train_dataset = Dataloader(split='train')
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
 
-    # For each key, check the type of its value and print some of its contents
-    for key in data.keys():
-        print(f"Type of data under key '{key}': {type(data[key])}")
-
-        # Check if the value is also a dictionary
-        if isinstance(data[key], dict):
-            print(f"Contents of the nested dictionary for key '{key}':")
-            for nested_key, nested_value in data[key].items():
-                print(f"  {nested_key}: {nested_value}")
-                # Break after a few items to avoid too much output
-                break
+for feat, scimg, mask, label, _ in train_dataloader:
+    print(f"Size of mask: {mask.size()}")
+    break  # Break after checking the first batch
 
