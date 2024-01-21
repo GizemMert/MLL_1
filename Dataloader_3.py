fc7d30602aedc41d8353af438c936052474f0676
GizemMert <gizem.mert12@gmail.com>
2024-01-21 13:57:00 +0100

    updated

--- Dataloader_3.py
+++ Dataloader_3.py
@@ -88,15 +88,15 @@ class Dataloader(Dataset):
 
     def __len__(self):
         if self.split == 'train':
-            return int(len(self.data) * 0.9)  # 90% for training
+            return int(len(self.data) * 1)  # 90% for training
         elif self.split == 'test':
-            return len(self.data) - int(len(self.data) * 0.9)  # 10% for testing
+            return len(self.data) - int(len(self.data) * 1)  # 10% for testing
 
     def __getitem__(self, index):
         if self.split == 'train':
-            index = index % int(len(self.data) * 0.9)
+            index = index % int(len(self.data) * 1)
         elif self.split == 'test':
-            index = int(len(self.data) * 0.9) + index
+            index = int(len(self.data) * 1) + index
 
         key = self.data[index]
         label_fold = self.samples[key]['label']
