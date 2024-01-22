0aba0b29e74cb52b9e6ce18e8aa1ddb0cb7f7715
GizemMert <gizem.mert12@gmail.com>
2024-01-22 08:41:22 +0100

    updated

--- Dataloader_3.py
+++ Dataloader_3.py
@@ -139,7 +139,7 @@ class Dataloader(Dataset):
         mask_dilation = cv2.dilate(mask_dilation, kernel, iterations=8)
         mask_dilation = mask_dilation.astype(np.float32)
         mask_dilation = cv2.resize(mask_dilation, (128, 128))
-        mask_dilation = cv2.GaussianBlur(mask_dilation, (5, 5), sigmaX=0)
+        mask_dilation = cv2.GaussianBlur(mask_dilation, (11, 11), sigmaX=0)
 
         if len(mask_dilation.shape) == 2:
             mask_dilation = mask_dilation[..., np.newaxis]
