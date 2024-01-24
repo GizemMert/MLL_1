eb791a5971338ed5fc99b1b1e2a4360ca777e4ea
GizemMert <gizem.mert12@gmail.com>
2024-01-24 20:11:23 +0100

    updated

--- gif2.py
+++ gif2.py
@@ -1,13 +1,31 @@
-from PIL import Image
-import torch
-import numpy as np
-from model4 import VariationalAutoencodermodel4, reparametrize
-from Dataloader_2 import Dataloader
 from torch.utils.data import DataLoader
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+import torch
+import umap
+from Dataloader_4 import Dataloader, label_map
+from model4 import VariationalAutoencodermodel4, reparametrize
+import os
+from matplotlib.gridspec import GridSpec
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.gaussian_process.kernels import WhiteKernel
+import geomstats.backend as gs
+import matplotlib.pyplot as plt
+from Beta_Visualization import Beta
+from geomstats.information_geometry.beta import BetaDistributions
+from geomstats.geometry.connection import Connection
+from geomstats.geometry.complex_manifold import ComplexManifold
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import geomstats.visualization as visualization
+from geomstats.geometry.special_euclidean import SpecialEuclidean
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+beta = BetaDistributions()
+beta_p = Beta()
+
 
 label_map = {
     'basophil': 0,
@@ -25,93 +43,64 @@ label_map = {
     'smudge_cell': 12,
 }
 
+if __name__ == '__main__':
+    inverse_label_map = {v: k for k, v in label_map.items()}  # inverse mapping for UMAP
+    batch_size = 128
+    num_classes = len(label_map)
+    epoch = 140
 
-def interpolate_gif_with_gpr(model, filename, features, n=100, latent_dim=30):
-    model.eval()
+    umap_dir = 'umap_manifold_path'
+    if not os.path.exists(umap_dir):
+        os.makedirs(umap_dir)
+
+    pdf_dir = 'pdf_manifold_path'
+    if not os.path.exists(pdf_dir):
+        os.makedirs(pdf_dir)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = VariationalAutoencodermodel4(latent_dim=30)
+    model_save_path = 'trained_model4cp2_new5.pth'
+    model.load_state_dict(torch.load(model_save_path, map_location=device))
+    model.to(device)
+    model.eval()
 
+    train_dataset = Dataloader(split='train')
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=False, num_workers=1)
 
-    def get_latent_vector(x):
-        distributions = model.encoder(x)
-        mu = distributions[:, :latent_dim]
-        logvar = distributions[:, latent_dim:]
-        z = reparametrize(mu, logvar)
-        return z
+    # Load all latent representations
+    latent_dir = 'latent_data4cp2_new5'
+    latents_path = os.path.join(latent_dir, f'latent_epoch_{epoch}.npy')
+    label_dir = 'label_data4cp2_new5'
+    labels_path = os.path.join(label_dir, f'label_epoch_{epoch}.npy')
 
-    # Generate the latent representations
-    latents = [get_latent_vector(feature.to(device)) for feature in features]
+    # Load all latent representations
+    latent_data = np.load(latents_path)
+    latent_data_reshaped = latent_data.reshape(latent_data.shape[0], -1)
+    print("Latent data shape:", latent_data_reshaped.shape)
 
+    # Load all labels
+    all_labels_array = np.load(labels_path)
+    print("Labels array shape:", all_labels_array.shape)
 
-    def slerp(val, low, high):
-        low_norm = low / torch.norm(low, dim=1, keepdim=True)
-        high_norm = high / torch.norm(high, dim=1, keepdim=True)
-        omega = torch.acos((low_norm * high_norm).sum(dim=1, keepdim=True).clamp(-1, 1))
-        so = torch.sin(omega)
-        res = torch.sin((1.0 - val) * omega) / so * low + torch.sin(val * omega) / so * high
-        return res.where(so != 0, low)
+    # print("Labels array shape:", all_labels_array.shape)
 
-    # Interpolate between the latent vectors
-    all_interpolations = []
-    for i in range(len(latents) - 1):
-        for t in np.linspace(0, 1, n):
-            z_interp = slerp(t, latents[i], latents[i + 1])
-            all_interpolations.append(z_interp)
+    # Filter out the 'erythroblast' class
+    erythroblast_class_index = label_map['erythroblast']
+    mask = all_labels_array != erythroblast_class_index
+    filtered_latent_data = latent_data_reshaped[mask]
+    filtered_labels = all_labels_array[mask]
 
+    reducer = umap.UMAP(n_components=3)
+    latent_data_3d = reducer.fit_transform(filtered_latent_data)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    scatter = ax.scatter(latent_data_3d[:, 0], latent_data_3d[:, 1], latent_data_3d[:, 2], c=filtered_labels, cmap='viridis')
 
-    interpolate_list = []
-    for z in all_interpolations:
-        y = model.decoder(z)
-        img = model.img_decoder(y)
-        img = img.permute(0, 2, 3, 1)
-        interpolate_list.append(img.squeeze(0).to('cpu').detach().numpy())
+    unique_labels = np.unique(filtered_labels)
+    color_map = plt.cm.get_cmap('viridis', len(unique_labels))  # Ensure correct color mapping
 
+    for idx, unique_label in enumerate(unique_labels):
+        ax.scatter([], [], [], color=color_map(idx), label=unique_label)
 
-    interpolate_list = [np.clip(img * 255, 0, 255).astype(np.uint8) for img in interpolate_list]
-
-    images_list = [Image.fromarray(img) for img in interpolate_list]
-
-
-    images_list[0].save(
-        f'{filename}.gif',
-        save_all=True,
-        append_images=images_list[1:],
-        loop=0,
-        duration=100
-    )
-
-# Load the model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = VariationalAutoencodermodel4(latent_dim=30)
-model_save_path = 'trained_model4cp2_new2.pth'
-model.load_state_dict(torch.load(model_save_path, map_location=device))
-model.to(device)
-model.eval()
-
-
-def get_images_from_different_classes(dataloader, class_1_label, class_2_label):
-    feature_1, feature_2 = None, None
-
-    for feature, _, _, labels, _ in dataloader:
-        if feature_1 is not None and feature_2 is not None:
-            break
-
-        for i, label in enumerate(labels):
-            if label.item() == class_1_label and feature_1 is None:
-                feature_1 = feature[i].unsqueeze(0)
-
-            if label.item() == class_2_label and feature_2 is None:
-                feature_2 = feature[i].unsqueeze(0)
-
-    return [feature_1, feature_2]
-
-
-train_dataset = Dataloader(split='train')
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=1)
-
-selected_features = get_images_from_different_classes(train_dataloader, label_map['myeloblast'], label_map['neutrophil_segmented'])
-
-# Convert to appropriate format and device
-selected_images = [feature.float().to(device) for feature in selected_features if feature is not None]
-
-# Now, you can use these images for your interpolation GIF
-interpolate_gif_with_gpr(model, "vae_interpolation_masked3", selected_images)
+    ax.legend()
+    plt.savefig('my_3d_latent.png', dpi=300)
