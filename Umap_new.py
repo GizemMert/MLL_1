874f773fe9f008e0ebcaac776e70dfa3f9db0372
GizemMert <gizem.mert12@gmail.com>
2024-01-25 00:00:03 +0100

    updated

--- Umap_new.py
+++ Umap_new.py
@@ -119,9 +119,9 @@ if __name__ == '__main__':
     if random_myeloblast_point is not None and random_neutrophil_banded_point is not None:
         if not (np.isnan(random_myeloblast_point).any() or np.isnan(random_neutrophil_banded_point).any()):
             if random_myeloblast_point.shape == (2,) and random_neutrophil_banded_point.shape == (2,):
-                t = np.linspace(0, 1, 20)
                 geod = beta.metric.geodesic(initial_point=random_myeloblast_point,
-                                            end_point=random_neutrophil_banded_point)(t)
+                                            end_point=random_neutrophil_banded_point,
+                                            color=cc)
 
                 # Print the geodesic points
                 print("Geodesic points:")
