c49762bec87950b68ddd2e7caeabf69e895dfe22
GizemMert <gizem.mert12@gmail.com>
2024-01-27 19:33:24 +0100

    updated

--- gif2.py
+++ gif2.py
@@ -4,6 +4,7 @@ import umap
 from Dataloader_4 import Dataloader, label_map
 from model4 import VariationalAutoencodermodel4, reparametrize
 import os
+import plotly.graph_objs as go
 from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 import numpy as np
@@ -60,7 +61,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = VariationalAutoencodermodel4(latent_dim=30)
     model_save_path = 'trained_model4cp2_new5.pth'
-    model.load_state_dict(torch.load(model_save_path, map_location=device))
+    # model.load_state_dict(torch.load(model_save_path, map_location=device))
     model.to(device)
     model.eval()
 
@@ -91,20 +92,35 @@ if __name__ == '__main__':
     filtered_labels = all_labels_array[mask]
     unique_labels = np.unique(filtered_labels)
 
+    # Perform UMAP dimensionality reduction to 3D
     reducer = umap.UMAP(n_components=3)
     latent_data_3d = reducer.fit_transform(filtered_latent_data)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    # Create a Plotly interactive 3D scatter plot
+    trace = go.Scatter3d(
+        x=latent_data_3d[:, 0],
+        y=latent_data_3d[:, 1],
+        z=latent_data_3d[:, 2],
+        mode='markers',
+        marker=dict(
+            size=5,
+            color=filtered_labels,  # Color by labels
+            colorscale='Viridis',  # Choose a colorscale
+            opacity=0.8
+        )
+    )
 
-    # Create a scatter plot with color mapping using 'Spectral' colormap
-    color_map = plt.get_cmap('Spectral')  # Updated to the correct colormap fetching method
-    norm = plt.Normalize(min(unique_labels), max(unique_labels))
+    data = [trace]
+    layout = go.Layout(
+        title="3D UMAP Visualization",
+        margin=dict(l=0, r=0, b=0, t=0),
+        scene=dict(
+            xaxis=dict(title='UMAP Dimension 1'),
+            yaxis=dict(title='UMAP Dimension 2'),
+            zaxis=dict(title='UMAP Dimension 3'),
+        )
+    )
 
-    scatter = ax.scatter(latent_data_3d[:, 0], latent_data_3d[:, 1], latent_data_3d[:, 2],
-                         c=filtered_labels, cmap=color_map, norm=norm, s=5)  # Adjust the size with `s`
-
-    # Create a colorbar for the scatter plot
-    cbar = plt.colorbar(scatter, ax=ax)
-    cbar.set_label('Label')
-    plt.savefig('my_3d_latent.png', dpi=300)
+    fig = go.Figure(data=data, layout=layout)
+    fig.show()
+    fig.write_html("your_plot.html")
