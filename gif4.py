183c5ef17ce551aed4e3ee451a96f1346db218cd
GizemMert <gizem.mert12@gmail.com>
2024-01-29 12:19:01 +0100

    updated

--- gif4.py
+++ gif4.py
@@ -20,7 +20,7 @@ import geomstats.geometry.complex_manifold as cm
 # dimension = 30
 # complex_manifold = cm.ComplexManifold(dimension)
 
-# normal = NormalDistributions(sample_dim=1)
+normal = NormalDistributions(sample_dim=1)
 epoch = 140
 latent_dim = 30
 
@@ -42,13 +42,13 @@ label_map = {
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = VariationalAutoencodermodel4(latent_dim=30)
-model_save_path = 'trained_model4cp2_new5.pth'
+model_save_path = '/Users/gizem/MLL_1/trained_model4cp2_new5 (1).pth'
 model.load_state_dict(torch.load(model_save_path, map_location=device))
 model.to(device)
 model.eval()
 
 
-"""
+
 # Load all latent representations
 latent_dir = 'latent_data4cp2_new5'
 latents_path = '/Users/gizem/MLL_1/latent_epoch_140 (1).npy'
@@ -57,8 +57,8 @@ labels_path = '/Users/gizem/MLL_1/label_epoch_140 (1).npy'
 
 # Load all latent representations
 latent_data = np.load(latents_path)
-latent_data_reshaped = latent_data.reshape(latent_data.shape[0], -1)
-print("Latent data shape:", latent_data_reshaped.shape)
+# latent_data_reshaped = latent_data.reshape(latent_data.shape[0], -1)
+print("Latent data shape:", latent_data.shape)
 
 # Load all labels
 all_labels_array = np.load(labels_path)
@@ -87,6 +87,84 @@ random_neutrophil_banded_index = np.random.choice(neutrophil_banded_indices)
 random_myeloblast_point = filtered_latent_data[random_myeloblast_index]
 random_neutrophil_banded_point = filtered_latent_data[random_neutrophil_banded_index]
 print("Poin data shape:", random_myeloblast_point.shape)
+
+def get_latent_vector(x):
+    distributions = model.encoder(x)
+    mu = distributions[:, :latent_dim]
+    logvar = distributions[:, latent_dim:]
+    z = reparametrize(mu, logvar)
+    return z
+
+def compute_geodesic_path(start_latent, end_latent, n_points=100):
+    print(f"Start latent shape: {start_latent.shape}")
+    print(f"End latent shape: {end_latent.shape}")
+
+    geodesic_ab_fisher = normal.metric.geodesic(start_latent, end_latent)
+    t = gs.linspace(0, 1, n_points)
+    geodesic_path = geodesic_ab_fisher(t)
+
+    print(f"Geodesic path shape: {geodesic_path.shape}")
+
+def interpolate_gif_pdf(filename, start_latent, end_latent, steps=100,  grid_size=(10, 10)):
+    print("Starting interpolate_gif_pdf function")
+    model.eval()
+
+    geodesic_path = compute_geodesic_path(start_latent.squeeze(), end_latent.squeeze(), steps)
+    print("Computed geodesic path")
+
+    decoded_images = []
+    for i, z in enumerate(geodesic_path):
+        print(f"Processing step {i+1}/{len(geodesic_path)}")
+
+        z_tensor = torch.from_numpy(z).float().to(device).unsqueeze(0)
+
+        # Print the shape of z_tensor to debug
+        print(f"Shape of z_tensor: {z_tensor.shape}")
+        with torch.no_grad():
+            decoded_img = model.decoder(z_tensor)
+            decoded_img = model.img_decoder(decoded_img)
+        decoded_images.append(decoded_img.cpu())
+
+    print(f"Total decoded images: {len(decoded_images)}")
+    while len(decoded_images) < grid_size[0] * grid_size[1]:
+        decoded_images.append(torch.zeros_like(decoded_images[0]))
+
+    decoded_images = decoded_images[:grid_size[0] * grid_size[1]]
+
+    tensor_grid = torch.stack(decoded_images).squeeze(1)  # Remove batch dimension if necessary
+    grid_image = make_grid(tensor_grid, nrow=grid_size[1], normalize=True, padding=2)
+    grid_image = ToPILImage()(grid_image)
+    grid_image.save('/path/to/directory/' + filename + '.jpg', quality=95)
+    print("Image saved successfully")
+
+
+interpolate_gif_pdf("vae_interpolation_grid_pdf", random_myeloblast_point, random_neutrophil_banded_point, steps=100, grid_size=(10, 10))
+
+
+def get_images_from_different_classes(dataloader, class_1_label, class_2_label):
+    feature_1, feature_2 = None, None
+
+    for feature, _, _, labels, _ in dataloader:
+        if feature_1 is not None and feature_2 is not None:
+            break
+
+        for i, label in enumerate(labels):
+            if label.item() == class_1_label and feature_1 is None:
+                feature_1 = feature[i].unsqueeze(0)
+
+            if label.item() == class_2_label and feature_2 is None:
+                feature_2 = feature[i].unsqueeze(0)
+
+    return [feature_1, feature_2]
+
+
+train_dataset = Dataloader(split='train')
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=1)
+
+selected_features = get_images_from_different_classes(train_dataloader, label_map['myeloblast'], label_map['neutrophil_banded'])
+
+start_latent, end_latent = [get_latent_vector(feature.float().to(device),) for feature in selected_features]
+
 """
 
 
@@ -130,7 +208,7 @@ def interpolate_centroids(centroid1, centroid2, steps=100):
 
 
 
-"""
+
 def to_complex_coordinates(latent_vector):
     real_part = latent_vector[:latent_vector.shape[0] // 2]
     imag_part = latent_vector[latent_vector.shape[0] // 2:]
@@ -187,7 +265,7 @@ def interpolate_gif_gpr(filename, start_latent, end_latent, steps=100, grid_size
     grid_image.save(filename + '.jpg', quality=95)
     print("Image saved successfully")
 
-"""
+
 
 def get_images_from_different_classes(dataloader, class_1_label, class_2_label):
     feature_1, feature_2 = None, None
@@ -283,7 +361,7 @@ start_latent, end_latent = [get_latent_vector(feature.float().to(device),) for f
 # Call the function with your data
 interpolate_grid_com("vae_interpolation_com", centroid_class_1, centroid_class_2, grid_size=(10, 10))
 
-""""
+
 def get_latent_vector(x):
     distributions = model.encoder(x)
     mu = distributions[:, :latent_dim]
@@ -361,6 +439,8 @@ train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=128, sh
 selected_features = get_images_from_different_classes(train_dataloader, label_map['myeloblast'], label_map['neutrophil_banded'])
 
 start_latent, end_latent = [get_latent_vector(feature.float().to(device),) for feature in selected_features]
+
 """
+
 
 
