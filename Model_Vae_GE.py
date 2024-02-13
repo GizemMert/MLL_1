42942b665b1bfdb6d66e9f6d60ff17b139c278c6
GizemMert <gizem.mert12@gmail.com>
2024-02-13 11:43:15 +0100

    new files added

---
+++ Model_Vae_GE.py
@@ -0,0 +1,68 @@
+import torch.nn as nn
+import torch
+from torch import Tensor
+from torch._inductor.ir import View
+from torch.autograd import Variable
+
+
+
+class VAE_GE(nn.Module):
+    def __init__(self, input_shape=None, latent_dim=30):
+        super(VAE_GE, self).__init__()
+        self.latent_dim = latent_dim
+        self.input_shape = input_shape
+        self.encoder = nn.Sequential(
+            nn.Linear(self.input_shape, 1024),
+            nn.ReLU(),
+            nn.BatchNorm1d(1024),
+
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.BatchNorm1d(512),
+
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.BatchNorm1d(256),
+
+            nn.Linear(256, 2 * self.latent_dim)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(self.latent_dim, 256),
+            nn.ReLU(),
+            nn.BatchNorm1d(256),
+
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.BatchNorm1d(512),
+
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.BatchNorm1d(1024),
+
+            nn.Linear(1024, self.input_shape),
+            nn.Sigmoid()
+
+        )
+
+    def forward(self, x):
+        distributions = self.encoder(x)
+        mu = distributions[:, :self.latent_dim]
+        logvar = distributions[:, self.latent_dim:]
+        z = reparametrize(mu, logvar)
+        y = self.decoder(z)
+
+        return z, y, mu, logvar
+
+class View(nn.Module):
+    def __init__(self, size):
+        super(View, self).__init__()
+        self.size = size
+
+    def forward(self, tensor):
+        return tensor.view(self.size)
+
+def reparametrize(mu, log_var):
+    std = log_var.div(2).exp()
+    eps = Variable(std.data.new(std.size()).normal_())
+    return mu + std * eps
