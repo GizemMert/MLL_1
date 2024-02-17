29739cc853a5f9971804580ff7867594e55937ed
GizemMert <gizem.mert12@gmail.com>
2024-02-17 17:03:13 +0100

    updated

--- train_ge_3.py
+++ train_ge_3.py
@@ -63,7 +63,7 @@ input_shape = X.shape[1]  # number of genes
 model = VAE_GE(input_shape=input_shape, latent_dim=50).to(device)
 
 
-optimizer = Adam(model.parameters(), lr=0.01)
+optimizer = Adam(model.parameters(), lr=0.001)
 
 
 latent_dir = 'latent_variables_GE_2'
