f270a2599e8a207c7e988e01a275d8dc9836d4fa
GizemMert <gizem.mert12@gmail.com>
2024-02-19 14:56:15 +0100

    updated

--- train_ge_1.py
+++ train_ge_1.py
@@ -56,7 +56,7 @@ scvi_tensor = torch.tensor(adata.obsm["X_scvi"], dtype=torch.float32)
 
 # Initialize dataset
 dataset = GeneExpressionDataset(X_tensor, label_tensor, scvi_tensor)
-dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=1)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
 print("loading done")
 heatmap_dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
 
