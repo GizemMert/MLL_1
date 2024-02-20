fcd04ce0c29dab7d531d89fb039250398469baff
GizemMert <gizem.mert12@gmail.com>
2024-02-20 14:11:13 +0100

    updated

--- job_script.sh
+++ job_script.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=150G
 #SBATCH --gres=gpu:1
-#SBATCH --time=48:00:00
+#SBATCH --time=1:00:00
 #SBATCH --nice=10000
 #SBATCH --qos=gpu_normal
 #SBATCH --constraint=a100_80gb
