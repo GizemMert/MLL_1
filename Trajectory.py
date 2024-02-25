8e9c180d8cbdb4663df2b98c3631a847920c85e2
GizemMert <gizem.mert12@gmail.com>
2024-02-26 00:10:53 +0100

    updated

--- Trajectory.py
+++ Trajectory.py
@@ -181,6 +181,7 @@ def get_images_from_different_classes(dataloader, class_1_label, class_2_label):
 
 def get_latent_vector(x):
     distributions = model_1.encoder(x)
+    print(f"Distributions shape: {distributions.shape}")
     mu = distributions[:, :latent_dim]
     logvar = distributions[:, latent_dim:]
     print(f"Mu shape: {mu.shape}")
