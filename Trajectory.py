b0e1950caca95a74496e112d153c0422cf975695
GizemMert <gizem.mert12@gmail.com>
2024-02-29 11:22:24 +0100

    updated

--- Trajectory.py
+++ Trajectory.py
@@ -245,7 +245,7 @@ final_expression = gen_expression[-1, :]
 abs_diff_per_gene = np.abs(final_expression - initial_expression)
 
 ptp_values = np.ptp(gen_expression, axis=0)
-threshold = np.max(ptp_values) * 0.08
+threshold = np.max(ptp_values) * 0.05
 
 
 variable_genes_indices = np.where(abs_diff_per_gene > threshold)[0]
