49d82fc66e5945f4ffd987842778adb0460036fb
GizemMert <gizem.mert12@gmail.com>
2024-03-04 16:52:57 +0100

    updated

--- job_script_2.sh
+++ job_script_2.sh
@@ -9,11 +9,11 @@
 #SBATCH --time=10:00:00
 #SBATCH --nice=10000
 #SBATCH --qos=gpu_normal
-#SBATCH --constraint=a100_80gb
+
 #SBATCH --output=my_job_output_2.log
 #SBATCH --error=my_job_error_2.log
 
 source /home/aih/gizem.mert/miniconda3/etc/profile.d/conda.sh
 conda activate my_env2
 
-python train4_cp5.py
+python Trajectory_lung_blood.py
