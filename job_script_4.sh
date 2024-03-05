5902a9d60510c7648661f76ce0a4e0a052a59d1d
GizemMert <gizem.mert12@gmail.com>
2024-03-05 14:59:20 +0100

    updated

--- job_script_4.sh
+++ job_script_4.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=save_img
-#SBATCH --partition=cpu_p
+#SBATCH --job-name=n_n
+#SBATCH --partition=gpu_p
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=6
 #SBATCH --mem=50G
 #SBATCH --gres=gpu:1
 #SBATCH --time=1:00:00
 #SBATCH --nice=1
-#SBATCH --qos=cpu_normal
-
-
+#SBATCH --qos=gpu_short
 #SBATCH --output=my_job_output_4.log
 #SBATCH --error=my_job_error_4.log
 
