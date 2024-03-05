d7ce363245b94fc031ee4c1da3e8affc363d75fd
GizemMert <gizem.mert12@gmail.com>
2024-03-05 13:43:23 +0100

    updated

--- gif5.py
+++ gif5.py
@@ -117,8 +117,8 @@ print("filtered neutrophil label shape:", filtered_labels_neutrophil_banded.shap
 print("filtered neutrophil label shape:", filtered_labels_neutrophil_segmented.shape)
 
 myeloblast_indices = np.where(filtered_labels == label_map['myeloblast'])[0]
-neutrophil_banded_indices = np.where(filtered_labels_neutrophil_banded == label_map['neutrophil_banded'])[0]
-neutrophil_seg_indices = np.where(filtered_labels_neutrophil_segmented == label_map['neutrophil_segmented'])[0]
+neutrophil_banded_indices = np.where(filtered_labels == label_map['neutrophil_banded'])[0]
+neutrophil_seg_indices = np.where(filtered_labels == label_map['neutrophil_segmented'])[0]
 basophil_indices = np.where(filtered_labels == label_map['basophil'])[0]
 eosinophil_indices = np.where(filtered_labels == label_map['eosinophil'])[0]
 monocyte_indices = np.where(filtered_labels == label_map['monocyte'])[0]
@@ -133,8 +133,8 @@ random_monocyte_index = np.random.choice(monocyte_indices)
 
 random_myeloblast_point = filtered_latent_data[random_myeloblast_index]
 # You can replace filtered_laten_data with neutrophil_data
-random_neutrophil_banded_point = n_banded_data[random_neutrophil_banded_index]
-random_neutrophil_seg_point = n_segment_data[random_neutrophil_seg_index]
+random_neutrophil_banded_point = filtered_latent_data[random_neutrophil_banded_index]
+random_neutrophil_seg_point = filtered_latent_data[random_neutrophil_seg_index]
 random_basophil_point = filtered_latent_data[random_basophil_index]
 random_eosinophil_point = filtered_latent_data[random_eosinophil_index]
 random_monocyte_point = filtered_latent_data[random_monocyte_index]
