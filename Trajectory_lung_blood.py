1eeee1ac8109608e29ed9e5dd615f3608ced2c02
GizemMert <gizem.mert12@gmail.com>
2024-03-05 12:14:50 +0100

    updated

--- Trajectory_lung_blood.py
+++ Trajectory_lung_blood.py
@@ -231,7 +231,7 @@ selected_features = get_images_from_different_classes(train_dataloader, label_ma
 start_latent, end_latent = [get_latent_vector(feature.float().to(device)) for feature in selected_features]
 # interpolate_gif_gpr("interpolation_img_ge", start_latent, end_latent, steps=100, grid_size=(10, 10), device=device)
 interpolate_gif_gpr(model_1, "grid_banded_segmented", random_neutrophil_banded_point, random_neutrophil_seg_point, steps=100, grid_size=(10, 10))
-
+"""
 #SEQUENCE DECODING and GENE EXPRESSED DETECTION
 adata = anndata.read_h5ad('s_data_feature_liver_lung.h5ad')
 interpolated_points = torch.load('interpolation_band_segmented_latent_points.pt')
@@ -448,7 +448,7 @@ plt.close()
 print("Clusters finished")
 
 
-"""
+
 plt.title("Gene Expression Profiles by Cluster")
 plt.xlabel("Time")
 plt.ylabel("Fold Change")
