b5db8fe50fae4d395fe6c677d11558bd1abd4ab6
GizemMert <gizem.mert12@gmail.com>
2024-03-07 23:05:35 +0100

    updated

--- Trajectory_lung_blood.py
+++ Trajectory_lung_blood.py
@@ -297,7 +297,7 @@ print("Number of points retained after filtering:", mask.sum())
 fold_changes = fold_changes[mask, :]
 
 plt.figure(figsize=(20, 10))
-color_for_genes = {'MPO': 'red'}
+color_for_genes = {'CD16': 'red'}
 default_color = 'gray'
 
 for i, gene_idx in enumerate(variable_genes_indices):
