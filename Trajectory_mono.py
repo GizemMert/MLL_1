86faec4008a435823adfa06af24d3b53a32d32fd
GizemMert <gizem.mert12@gmail.com>
2024-03-12 12:23:32 +0100

    updated

--- Trajectory_mono.py
+++ Trajectory_mono.py
@@ -291,13 +291,13 @@ plt.figure(figsize=(20, 10))
 color_for_genes = {'RUNX1T1': 'red', 'CXCR2': 'yellow', 'LEF1': 'blue',
                    'CD163': 'pink'}
 default_color = 'gray'
-"""
+
 for i, gene_idx in enumerate(variable_genes_indices):
     gene_name = gene_names[gene_idx]
     if gene_name not in color_for_genes:
 
         plt.plot(range(fold_changes.shape[0]), fold_changes[:, i], color=default_color, alpha=0.2)
-"""
+
 for i, gene_idx in enumerate(variable_genes_indices):
     gene_name = gene_names[gene_idx]
     if gene_name in color_for_genes:
